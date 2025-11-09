793cececddd9e1d19d574cfbf81888447ea9a768
Elam Bravo <bravoelam@gmail.com>
2025-11-09 12:28:28 +0100

    Solved Exercise 12

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,8 +1,37 @@
 package com.campusdual.classroom;
 
 public class Exercise {
+
     public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println(myCar.isTachometerEqualToZero());
+        myCar.start();
+        myCar.stop();
+        myCar.start();
 
+        for (int i = 0; i < 4; i++) {
+            myCar.accelerate();
+        }
+
+        for (int i = 0; i < 6; i++) {
+            myCar.accelerate();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            myCar.accelerate();
+
+            for (int j = 0; j < 10; j++) { // ← variable cambiada de i a j
+                myCar.brake();
+            }
+
+            myCar.turnAngleOfWheels(20);
+            myCar.turnAngleOfWheels(60);
+
+            myCar.stop();
+            myCar.start();
+            myCar.accelerate();
+            myCar.setReverse(true);
+        }
     }
-
 }
+
